967c9602dad0abef1db1710948c0bcc90f0fc69e
AlexOBarnes <a.o.barnes222@gmail.com>
2024-08-27 14:02:35 +0100

    Added docstrings

--- 2021/day-1/coding_problem_26_08.py
+++ 2021/day-1/coding_problem_26_08.py
@@ -1,10 +1,12 @@
-
+'''Solution to advent of code 2021 day 1'''
 
 def get_data():
+    '''Returns data from text file'''
     with open("length.txt","r") as f:
         return [int(line.strip()) for line in f.readlines()]
 
 def process_data(nums):
+    '''Calculates how many are larger'''
     larger= 0
     for num1,num2 in zip(nums,nums[1:]):
         if num1<num2:
@@ -12,6 +14,7 @@ def process_data(nums):
     return larger
 
 def process_triplets(nums):
+    '''Calculates how many triplets are larger in sequence'''
     larger = 0
     for num1,num4 in zip(nums,nums[3:]):
         if (num1) < (num4):
